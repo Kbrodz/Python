be287fd3a843bbc19d5e138a939b9d430c814d81
Konrad Brodziak <48966539+Kbrodz@users.noreply.github.com>
2021-09-24 15:00:42 +0200

    Add files via upload

--- 2.WorldBank_Analyzer.py
+++ 2.WorldBank_Analyzer.py
@@ -5,16 +5,18 @@
     "author": "Konrad Brodziak"
 """
 
-from bs4 import BeautifulSoup, BeautifulStoneSoup as bs
+from math import isnan
+from bs4 import BeautifulStoneSoup as bs
 import pandas as pd
 import pmdarima as pm
 from pmdarima.model_selection import train_test_split
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 xml_source=open('WorldBank.xml').read()
 
 def main():
+    '''A method that analyse the data from World Bank'''
     #Create reverse of list of `dates_list`
     wbd=bs(xml_source).findAll('wb:date')
     dates_list = [list.get_text() for list in wbd][::-1]
@@ -23,26 +25,18 @@ def main():
     wbv=bs(xml_source).findAll('wb:value')
     values_list = [list.get_text() for list in wbv][::-1]
 
-    df=pd.DataFrame({'Date': dates_list, 'Values': values_list})
-
-    df_head=df.head()
-    df_tail=df.tail()
-
-    split_point = round(len(df_head)*(3/4))
-    df1_train = df_head[:split_point]
-    len_train = len(df1_train)
-    df1_test = df_head[split_point:]
-    len_test = len(df1_test)
+    df=pd.DataFrame({'Date': dates_list, 'Values': values_list}).to_numpy()
+    df=np.where(df == '', '0', df) #replace '' to '0'
+    #print(df[:, 1])
     
-    arima1 = pm.auto_arima(df1_train.Values, start_p=1, start_q=1,
-                      max_p=3, max_q=3, # maximum p and q
-                      m=10,              # frequency of series
-                      seasonal=False,   # No Seasonality
-                      start_P=0, 
-                      suppress_warnings=True, 
-                      stepwise=True, 
-                      maxiter = 100, )
-    print(arima1.summary())
+    train, test= train_test_split(df[:, 1], train_size=30)
+    model=pm.auto_arima(train, seasonal=True, m=12)
+    forecasts = model.predict(test.shape[0])
+    x = np.arange(df[:, 1].shape[0])
+
+    plt.plot(x[:30], train, c='blue')
+    plt.plot(x[30:], forecasts, c='green')
+    plt.show()
 
 if __name__ == '__main__':
     main()
